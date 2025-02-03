9dee80014495fa891d49044fc9df29b3bd7e81a9
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-02-03 19:18:25 +0100

    Initial Commit

---
+++ ZEmpleados_SUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZEmpleados_SUI
+//
+//  Created by Miguel Gallego on 3/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZEmpleados_SUI/ZEmpleados_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZEmpleados_SUIApp.swift
+//  ZEmpleados_SUI
+//
+//  Created by Miguel Gallego on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ZEmpleados_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
