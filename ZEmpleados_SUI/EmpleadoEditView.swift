11c8bf0678bdd0ea8e95d8b7f260584b85200f59
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-02-07 00:58:30 +0100

    EmpleadoEditView in progress

--- ZEmpleados_SUI/EmpleadoEditView.swift
+++ ZEmpleados_SUI/EmpleadoEditView.swift
@@ -1,20 +1,63 @@
-//
 //  EmpleadoEditView.swift
 //  ZEmpleados_SUI
-//
 //  Created by Miguel Gallego on 6/2/25.
-//
 
 import SwiftUI
 
 struct EmpleadoEditView: View {
     @ObservedObject var editVM: EmpleadoEditVM
     
+    @State var isDataError = false
+    
     var body: some View {
-        Text(editVM.empleado.fullName)
+        Form {
+            Section {
+                VStack(alignment: .leading) {
+                    Text("Nombre")
+                        .font(.headline)
+                    HStack {
+                        TextField("Nombre", text: $editVM.firstName)
+                        Button {
+                            editVM.firstName = ""
+                        } label: {
+                            Image(systemName: "xmark.circle.fill")
+                        }
+                        .buttonStyle(.plain)
+                        .opacity(editVM.firstName.isEmpty ? 0 : 0.5)
+                    }
+                    .padding(10)
+                    .background {
+                        Color(white: 0.95)
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(lineWidth: 2)
+                            .fill(.red)
+                            .opacity(isDataError ? 1 : 0)
+                    }
+                    Text("Nombre no puede estar vacío")
+                        .font(.caption).bold().foregroundStyle(.red)
+                        .opacity(isDataError ? 1 : 0)
+                }
+                .onChange(of: editVM.firstName) {
+                    if editVM.firstName.isEmpty {
+                        isDataError = true
+                    } else {
+                        isDataError = false
+                    }
+                }
+            } header: {
+                Text("Datos personales")
+            }
+        }
+        .navigationTitle("Editar empleado")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    EmpleadoEditView(editVM: EmpleadoEditVM(empleado: .testInstance))
+    NavigationStack {
+        EmpleadoEditView(editVM: EmpleadoEditVM(empleado: .testInstance))
+    }
 }
--- ZEmpleados_SUI/ViewModel/EmpleadoEditVM.swift
+++ ZEmpleados_SUI/ViewModel/EmpleadoEditVM.swift
@@ -7,7 +7,25 @@ import Foundation
 final class EmpleadoEditVM: ObservableObject {
     let empleado: Empleado
     
+    @Published var firstName = ""
+    @Published var lastName = ""
+    @Published var username = ""
+    @Published var email = ""
+    @Published var address = ""
+    @Published var zipcode = ""
+    @Published var gender = ""
+    @Published var department = ""
+
     init(empleado: Empleado) {
         self.empleado = empleado
+        
+        firstName = empleado.firstName
+        lastName = empleado.lastName
+        email = empleado.email
+        address = empleado.address
+        zipcode = empleado.zipcode
+        username = empleado.username
+        gender = empleado.gender.rawValue
+        department = empleado.department.rawValue
     }
 }
